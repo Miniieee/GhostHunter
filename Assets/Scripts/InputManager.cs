edd8e811784fe0bc54bad0c99f0e357af3564553
Miniieee <bagyi.tomi93@gmail.com>
2024-05-17 18:19:05 +0100

    FirstPersonController

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerControls playerControls;
+    private static InputManager instance;
+
+
+    public static InputManager Instance
+    { 
+        get 
+        {
+            return instance; 
+        } 
+    }
+
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+
+        playerControls = new PlayerControls();
+    }
+
+
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Move.ReadValue<Vector2>();
+    }
+
+    public Vector2 GetMouseDelta()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+
+    private void OnDisable()    
+    {
+        playerControls.Disable();
+    }
+}
