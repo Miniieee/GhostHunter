0ea20e1b180717119379b65e1dc684641e56c265
Miniieee <bagyi.tomi93@gmail.com>
2024-06-01 00:42:56 +0100

    SO list and SO

---
+++ Assets/Scripts/ScriptableObjectsScripts/EquipmentSO.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EquipmentSO", menuName = "ScriptableObjects/EquipmentSO")]
+public class EquipmentSO : ScriptableObject
+{
+    public GameObject equipmentPrefab;
+    public string equipmentName;
+
+}
