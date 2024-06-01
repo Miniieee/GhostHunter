9f64476dc81d3f93b0f4c0187ef309167689e735
Miniieee <bagyi.tomi93@gmail.com>
2024-06-01 13:22:25 +0100

    Equipments Spawned on network

--- Assets/Scripts/EquipmentNetworkSpawner.cs
+++ Assets/Scripts/EquipmentNetworkSpawner.cs
@@ -1,15 +1,26 @@
+using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
 public class EquipmentNetworkSpawner : NetworkBehaviour
 {
-    [SerializeField] private GameObject equipmentPrefab;
+    [SerializeField] private EquipmentListSO equipmentList;
+    [SerializeField] private List<Transform> equipmentListTransform;
+    private List<EquipmentSO> equipmentSOs = new List<EquipmentSO>();
     public override void OnNetworkSpawn()
     {
+        for (int i = 0; i < equipmentList.equipmentSOList.Count; i++)
+        {
+            equipmentSOs.Add(equipmentList.equipmentSOList[i]);
+        }
+
         if (!IsServer) return;
 
-        GameObject spawnedObject = Instantiate(equipmentPrefab, transform.position, transform.rotation);
-        spawnedObject.GetComponent<NetworkObject>().Spawn();
+        for (int i = 0; i < equipmentSOs.Count; i++)
+        {
+            GameObject spawnedObject = Instantiate(equipmentSOs[i].equipmentNetworkPrefab.gameObject, equipmentListTransform[i].position, equipmentListTransform[i].rotation);
+            spawnedObject.GetComponent<NetworkObject>().Spawn();
+        }
 
     }
 }
--- Assets/Scripts/ScriptableObjectsScripts/EquipmentSO.cs
+++ Assets/Scripts/ScriptableObjectsScripts/EquipmentSO.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "EquipmentSO", menuName = "ScriptableObjects/EquipmentSO")]
 public class EquipmentSO : ScriptableObject
 {
+    public GameObject equipmentNetworkPrefab;
     public GameObject equipmentPrefab;
     public string equipmentName;
 
