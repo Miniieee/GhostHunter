1aec01ffbe92cd5753e0758d9f0b6ccfd5769650
Miniieee <106557722+Miniieee@users.noreply.github.com>
2024-10-20 21:01:32 +0100

    Added playerviusal settings
    Sets the owner player visual to Hide Layer, now it won't be seeing it's own body, but still shares the transform information with other players

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,5 +3,46 @@ using Unity.Netcode;
 
 public class Player : NetworkBehaviour
 {
-    
+    private CameraRotationFollower playerVisuals;
+
+    private void Awake()
+    {
+        playerVisuals = GetComponentInChildren<CameraRotationFollower>();
+    }
+
+    private void Start()
+    {
+        if (IsOwner)
+        {
+            int layerNumber = LayerMask.NameToLayer("Hide");
+            SetLayerRecursively(playerVisuals.gameObject, layerNumber);
+        }
+        else
+        {
+            int layerNumber = LayerMask.NameToLayer("Player");
+            SetLayerRecursively(playerVisuals.gameObject, layerNumber);
+        }
+    }
+
+
+    private void SetLayerRecursively(GameObject obj, int newLayer)
+    {
+        if (obj == null)
+        {
+            return;
+        }
+
+        // Set the layer of the current object
+        obj.layer = newLayer;
+
+        // Recursively set the layer on all child objects
+        foreach (Transform child in obj.transform)
+        {
+            if (child == null)
+            {
+                continue;
+            }
+            SetLayerRecursively(child.gameObject, newLayer);
+        }
+    }
 }
