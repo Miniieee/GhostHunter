c0c839a722ebc48e628cb9fe72ffca73e043f31a
Miniieee <106557722+Miniieee@users.noreply.github.com>
2024-11-28 23:29:05 +0000

    added sphere collider and logic

--- Assets/Scripts/Equipments/EmfActivate.cs
+++ Assets/Scripts/Equipments/EmfActivate.cs
@@ -1,3 +1,4 @@
+using System;
 using Interfaces;
 using UnityEngine;
 
@@ -5,13 +6,22 @@ namespace Equipments
 {
     public class EmfActivate : MonoBehaviour, IActivatable
     {
-        bool emfToggle = false;
+        private bool emfToggle = false;
+        private SphereCollider sphereCollider;
+        
+        private void Start()
+        {
+            sphereCollider = GetComponent<SphereCollider>();
+            sphereCollider.enabled = false;
+        }
+
         public void Activate(ulong networkObjectId)
         {
             emfToggle = !emfToggle;
             if (emfToggle)
             {
                 Debug.Log("Emf is on player ID: " + networkObjectId);
+                sphereCollider.enabled = true;
                 //turn on emf lights
                 //activates sphere collider
                 //on collision with ghost, ghost will be detected based on its distance and activity level
@@ -19,8 +29,19 @@ namespace Equipments
             else
             {
                 Debug.Log("Emf is off player ID: " + networkObjectId);
+                sphereCollider.enabled = false;
                 //turn off emf lights
                 //deactivates sphere collider
+            }
+        }
+        
+        private void OnTriggerStay(Collider other)
+        {
+            if (other.gameObject.CompareTag("Ghost"))
+            {
+                Debug.Log("Ghost detected");
+                float distance = Vector3.Distance(transform.position, other.transform.position);
+                Debug.Log("Distance: " + distance);
             }
         }
     }
